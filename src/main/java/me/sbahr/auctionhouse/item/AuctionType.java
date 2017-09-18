2e6b0cf27aa753b9b9fb3db6801d4f0681374762
Stephen Bahr <sbahr@bu.edu>
2017-09-18 01:05:00 -0400

    - Adds AuctionType, an enum that represents the type of auctions.

---
+++ src/main/java/me/sbahr/auctionhouse/item/AuctionType.java
@@ -0,0 +1,10 @@
+package me.sbahr.auctionhouse.item;
+
+public enum AuctionType {
+	
+	/** Represents an item that is being bought */
+	BUY,
+	/** Represents an item that is being sold */
+	SELL();
+
+}
