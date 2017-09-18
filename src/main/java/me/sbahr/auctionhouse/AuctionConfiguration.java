93a6c5c412cb0fd80ed1b52f66a2a1e0cf9df5fc
Stephen Bahr <sbahr@bu.edu>
2017-09-18 01:03:54 -0400

    - Adds AuctionConfiguration, a configuration file that is used so we can change values down the road.

---
+++ src/main/java/me/sbahr/auctionhouse/AuctionConfiguration.java
@@ -0,0 +1,10 @@
+package me.sbahr.auctionhouse;
+
+public class AuctionConfiguration {
+	
+	/**
+	 * The number of auction items to display per page.
+	 */
+	public static final int AUCTION_ITEMS_PER_PAGE = 45;
+
+}
